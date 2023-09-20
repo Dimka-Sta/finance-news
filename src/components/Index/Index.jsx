92922fc0fc4c2a8ff69eee2d2af62bd5e2ad49c7
dimka_sta <dimka.sta777@gmail.com>
2023-09-20 10:41:03 +0200

    add axios

--- src/components/Index/Index.jsx
+++ src/components/Index/Index.jsx
@@ -1,7 +1,11 @@
 import { useEffect, useState } from 'react';
 import Card from './Card';
+import { db } from '../../firebase';
+import axios from 'axios';
 
 const Index = () => {
+
+
 
     const [posts, setPosts] = useState([]);
 
@@ -13,15 +17,19 @@ const Index = () => {
             .then((json) => setPosts(json))
     }, []);
 
+    useEffect(() => {
+        axios.get('https://financial-news-ecaa8-default-rtdb.europe-west1.firebasedatabase.app/')
+            .then((resp) => console.log(resp, 'ewretbr'))
+    }, [])
+
     const newPosts = posts.map((post, index) => ({
         ...post,
         id: index + 1
     }));
 
-
     return (
         <div className='app-content'>
-            
+
             {newPosts.map(({ title, body, id, imageUrl }) => {
                 return <Card title={title} body={body} key={id} id={id} imageUrl={imageUrl} />
             })}
